542b18f423e82df810f52c66c237bcc913d7f8c4
pienvedejs <renate.vitina@gmail.com>
2022-02-24 20:12:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ 
+
+
+*/
+using System;
+
+string HeroName = "Brokolis";
+int HeroAge = '23';
+int HeroGender = 'm';
+
+
+namespace SuperHero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("")
+        }
+    }
+}
